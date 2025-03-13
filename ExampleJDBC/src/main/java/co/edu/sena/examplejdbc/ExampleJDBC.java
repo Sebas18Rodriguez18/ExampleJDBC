cb3c5022199ca5c735750bf3f5451ada54827374
Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-03-13 08:10:28 -0500

    Configuación del Proyecto
    Se añadió la dependencia de MySQL connector j al pom y se agregan tags de documentación

---
+++ ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/ExampleJDBC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.sena.examplejdbc;
+
+/**
+ * Fecha: 13/03/2025
+ * @author Sebastian Rodriguez
+ * Descripcion: Ejemplo de conexion MySQL con JDBC
+ */
+public class ExampleJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
